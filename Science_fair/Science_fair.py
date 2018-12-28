c9848ee06cc1ea7d3f7816a14dc5f1b79c1f02e3
caleb Hortman <caleb@Hortman.org>
2018-12-28 17:40:34 -0500

    final changes and data

--- Science_fair/Science_fair.py
+++ Science_fair/Science_fair.py
@@ -1,47 +1,93 @@
 import cv2
 import sys
- 
-(major_ver, minor_ver, subminor_ver) = (cv2.__version__).split('.')
- 
-if __name__ == '__main__' :
- 
-	# Set up tracker.
-	# Instead of MIL, you can also use
- 
-	tracker_types = ['BOOSTING', 'MIL','KCF', 'TLD', 'MEDIANFLOW', 'GOTURN', 'MOSSE', 'CSRT']
-	tracker_type = tracker_types[0]
+import os
+import csv
 
-	if int(minor_ver) < 3:
-		tracker = cv2.Tracker_create(tracker_type)
-	else:
-		if tracker_type == 'BOOSTING':
-			tracker = cv2.TrackerBoosting_create()
-		if tracker_type == 'MIL':
-			tracker = cv2.TrackerMIL_create()
-		if tracker_type == 'KCF':
-			tracker = cv2.TrackerKCF_create()
-		if tracker_type == 'TLD':
-			tracker = cv2.TrackerTLD_create()
-		if tracker_type == 'MEDIANFLOW':
-			tracker = cv2.TrackerMedianFlow_create()
-		if tracker_type == 'GOTURN':
-			tracker = cv2.TrackerGOTURN_create()
-		if tracker_type == 'MOSSE':
-			tracker = cv2.TrackerMOSSE_create()
-		if tracker_type == "CSRT":
-			tracker = cv2.TrackerCSRT_create()
 
-	folder = "c:/data/SF/vot2016/ball1/"
-	filename = folder + "00000001.jpg"
+tracker_types = ['KCF','TLD', 'MEDIANFLOW', 'MOSSE']
 
+
+def get_info(file):
+
+	f = open(file)
+	reader = csv.reader(f)
+	for row in reader:
+		x1 = float(row[0])
+		y1 = float(row[1])
+		x2 = float(row[2])
+		y2 = float(row[3])
+		x3 = float(row[4])
+		y3 = float(row[5])
+		x4 = float(row[6])
+		y4 = float(row[7])
+		return (x1,y1,x2,y2,x3,y3,x4,y4)
+
+
+
+def createtracker(type):
+
+	
+	tracker_type = tracker_types[type]
+	
+	if tracker_type == 'BOOSTING':
+		tracker = cv2.TrackerBoosting_create()
+	if tracker_type == 'MIL':
+		tracker = cv2.TrackerMIL_create()
+	if tracker_type == 'KCF':
+		tracker = cv2.TrackerKCF_create()
+	if tracker_type == 'TLD':
+		tracker = cv2.TrackerTLD_create()
+	if tracker_type == 'MEDIANFLOW':
+		tracker = cv2.TrackerMedianFlow_create()
+	if tracker_type == 'GOTURN':
+		tracker = cv2.TrackerGOTURN_create()
+	if tracker_type == 'MOSSE':
+		tracker = cv2.TrackerMOSSE_create()
+	if tracker_type == "CSRT":
+		tracker = cv2.TrackerCSRT_create()
+
+
+	return (tracker,tracker_type)
+
+
+
+
+def producebox(corners):
+	
+
+	x1 = corners[0]
+	y1 = corners[1]
+	x2 = corners[2]
+	y2 = corners[3]
+	x3 = corners[4]
+	y3 = corners[5]
+	x4 = corners[6]
+	y4 = corners[7]
+
+	left = min ([x1,x2,x3,x4])
+	top = min ([y1,y2,y3,y4])
+
+	right = max ([x1,x2,x3,x4])
+	bottom = max ([y1,y2,y3,y4])
+
+
+	H = (bottom - top) + 1
+	W = (right - left) + 1
+
+	bbox = (left,top,W,H)
+	return bbox
+
+
+def runalgorithm(folder,bbox,trackertype):
+
+	tracker, tracker_type = createtracker(trackertype)
+
+	f= open(folder + tracker_type + ".output" ,"w+")
+
+	filename = folder + "00000001.jpg" 
+
+	
 	frame = cv2.imread(filename)
-
-	# Define an initial bounding box
-	#bbox = (287, 23, 86, 320)
-	bbox = (483,411,65,50)
-
-	# Uncomment the line below to select a different bounding box
-	#bbox = cv2.selectROI(frame, False)
 
 
 	# Initialize tracker with first frame and bounding box
@@ -49,8 +95,15 @@ if __name__ == '__main__' :
 
 	fileindex = 2
 
+
+
+
 	while True:
-		filename = folder + "%08d.jpg" % fileindex
+		
+		Running_output = str(fileindex) + ","
+
+		filename = folder + "%08d.jpg" %fileindex
+
 		fileindex = fileindex + 1
 
 		# Read a new frame
@@ -66,6 +119,7 @@ if __name__ == '__main__' :
 
 		# Calculate Frames per second (FPS)
 		fps = cv2.getTickFrequency() / (cv2.getTickCount() - timer)
+		Running_output = Running_output + str(fps) + ","
 
 		# Draw bounding box
 		if ok:
@@ -73,9 +127,21 @@ if __name__ == '__main__' :
 			p1 = (int(bbox[0]), int(bbox[1]))
 			p2 = (int(bbox[0] + bbox[2]), int(bbox[1] + bbox[3]))
 			cv2.rectangle(frame, p1, p2, (255,0,0), 2, 1)
+
+
+
+
+			Running_output = Running_output + "%d,%d,%d,%d" %(bbox[0], bbox[1], bbox[2], bbox[3])
+
+
+			#output rectangle to file
+
 		else :
 			# Tracking failure
 			cv2.putText(frame, "Tracking failure detected", (100,80), cv2.FONT_HERSHEY_SIMPLEX, 0.75,(0,0,255),2)
+			Running_output = Running_output = Running_output + "-1,-1,-1,-1" 
+
+			#output failed to file
 
 		# Display tracker type on frame
 		cv2.putText(frame, tracker_type + " Tracker", (100,20), cv2.FONT_HERSHEY_SIMPLEX, 0.75, (50,170,50),2)
@@ -89,3 +155,63 @@ if __name__ == '__main__' :
 		# Exit if ESC pressed
 		k = cv2.waitKey(1) & 0xff
 		if k == 27 : break
+	
+
+
+		f.write(Running_output + " \n")
+
+
+	f.close()
+
+
+
+
+		
+
+
+
+
+
+
+
+
+
+
+
+topfolder = "D:/Programing_project/vot2016/"
+subdir = os.listdir(topfolder)
+
+
+
+
+	
+
+for y in subdir:
+
+
+	if y == "singer2": continue
+	
+	#folder = topfolder + "singer2/" 
+	folder = topfolder + y + "/" 
+	ground_file = folder + "groundtruth.txt"
+
+	#folder = topfolder + "ball1/"
+
+	#corners = (496,419,536,419,536,461,496,461)
+	corners = get_info(ground_file)
+
+
+
+	for x in range (0, len(tracker_types)):
+
+		bbox = producebox(corners)
+
+		runalgorithm(folder,bbox,x)
+	#break
+
+	
+
+
+
+
+
---
+++ Science_fair/data.py
@@ -0,0 +1,176 @@
+
+import csv
+import math
+import os
+import cv2
+
+
+tracker_types = ['KCF','TLD', 'MEDIANFLOW', 'MOSSE']
+
+
+def compare (data,truth,folder):
+
+	DataW = data[2]
+	DataH = data[3]
+	Datax = data[0]
+	Datay = data[1]
+
+
+	truthW = truth[2]
+	truthH = truth[3]
+	truthx = truth[0]
+	truthy = truth[1]
+
+
+
+	centertx = truthx + (truthW/2)
+	centerty = truthy + (truthH/2)
+
+	centerdx = Datax + (DataW/2)
+	centerdy = Datax + (DataH/2)
+
+
+
+
+
+
+	cent_dist = math.sqrt((centerdx  - centertx)**2 + (centerdy -centerty)**2)
+
+
+
+
+
+
+	image_file = folder + "00000001.jpg" 
+
+
+	frame = cv2.imread(image_file)
+	height, width = frame.shape[:2]
+
+
+
+
+
+	diag = math.sqrt(width**2 + height**2)
+
+
+	return (cent_dist / diag *100) 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+def producebox(corners):
+	
+
+	x1 = corners[0]
+	y1 = corners[1]
+	x2 = corners[2]
+	y2 = corners[3]
+	x3 = corners[4]
+	y3 = corners[5]
+	x4 = corners[6]
+	y4 = corners[7]
+
+	left = min ([x1,x2,x3,x4])
+	top = min ([y1,y2,y3,y4])
+
+	right = max ([x1,x2,x3,x4])
+	bottom = max ([y1,y2,y3,y4])
+
+
+	H = (bottom - top) + 1
+	W = (right - left) + 1
+
+	bbox = (left,top,W,H)
+	return bbox
+
+
+
+summary = open("D:/Programing_project/Science_fair/results.txt","w+")
+
+topfolder = "D:/Programing_project/vot2016/"
+subdir = os.listdir(topfolder)
+
+
+
+for y in subdir:
+
+
+	if y == "singer2": continue
+
+
+	folder = topfolder + y + "/" 
+	ground_file = folder + "groundtruth.txt"
+	truth = open(ground_file)
+
+	truth_rects = []
+	reader = csv.reader(truth)
+	for row in reader:
+		x1 = float(row[0])
+		y1 = float(row[1])
+		x2 = float(row[2])
+		y2 = float(row[3])
+		x3 = float(row[4])
+		y3 = float(row[5])
+		x4 = float(row[6])
+		y4 = float(row[7])
+
+		truths = (x1,y1,x2,y2,x3,y3,x4,y4)
+		truth_rect = producebox(truths)
+
+		truth_rects.append(truth_rect)
+
+
+	for tracker_type in tracker_types:
+	
+		data = []
+
+
+		datafile= open(folder + tracker_type + ".output" ,"r")
+		reader = csv.reader(datafile)
+		for row in reader:
+			frame = int(row[0])
+			fps = float(row[1])
+			L = float(row[2])
+			T = float(row[3])
+			W = float(row[4])
+			H = float(row[5])
+
+
+			if L != -1: 
+				pos = compare ((L,T,W,H),truth_rects[frame -1],folder)
+				data.append((frame,fps,pos))
+
+
+		fps_sum = 0.0
+		pos_sum = 0.0
+
+		for d in data:
+			
+			fps_sum = fps_sum + d[1]
+
+
+			pos_sum = pos_sum + d[2]
+
+
+		fps_avg = 0
+		pos_avg = 0 
+
+		if len(data) != 0:
+			fps_avg = (fps_sum/len(data))
+			pos_avg = (pos_sum/len(data))
+	
+
+		summary.write ("%s %s fps:%f pos:%f\n" %(y,tracker_type,fps_avg,pos_avg))
+		print("%s %s fps:%f pos:%f" %(y,tracker_type,fps_avg,pos_avg))
+summary.close()
